7167a5009f96b734238c925fe8d7ad7b7c6b5e90
Max Witt <kirayatail@gmail.com>
2016-05-02 21:59:39 +0200

    rounding the pledged sum

--- app/directives/goalmeter.js
+++ app/directives/goalmeter.js
@@ -8,7 +8,7 @@ app.directive('goalMeter', ['$http', function($http) {
     link: function(scope, elem, attrs) {
       $http.get('/api/goals').then(function(res) {
         var goals = res.data.goals;
-        scope.sum = res.data.sum;
+        scope.sum = Math.round(res.data.sum);
         var max = Math.max((res.data.max / 0.8), scope.sum);
         for(var i in goals) {
           goals[i].height = 100*goals[i].amount / max;

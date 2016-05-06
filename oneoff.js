626c7ad24e4b037d4fe3665bc705927fa63ccca8
Max Witt <kirayatail@gmail.com>
2016-05-06 17:16:13 +0200

    test paymentdetails 3

--- oneoff.js
+++ oneoff.js
@@ -11,17 +11,23 @@ var Pledge = mongoose.model('Pledge');
 var wordlist = fs.readFileSync('wordlist').toString().trim().split(/[\r\n]/g);
 
 Pledge.find({}, function(err, pledges) {
+  var tasks = [];
 
   _.forEach(pledges, p => {
     if(!p.paymentToken) {
       p.paymentToken = wordlist.pop();
       p.paid = false;
+      console.log("Adding token to ", p.email);
+      tasks.push(p.save());
     }
-    mail.payment(p);
   });
 
-  pledges.save(function(err) {
+  Promise.all(tasks).then(function(res) {
+    console.log("Done saving!");
+    console.log(res);
+    process.exit();
+  }, function(err) {
     console.error(err);
     process.exit();
-  });
+  })
 });

02027c1b90b4e445e4fd71ae199cd1cabca1afa8
Max Witt <kirayatail@gmail.com>
2016-05-09 00:59:57 +0200

    visualize backers who have paid

--- app/directives/backerlist.js
+++ app/directives/backerlist.js
@@ -2,7 +2,7 @@ var app = angular.module('horvtavla');
 
 app.directive('backerList', function(){
   return {
-    template: '<h2>Tack till:</h2><ul class="backerlist"><li ng-repeat="b in backers">{{b}}</li><li ng-if="anonymous > 0">and {{anonymous}} anonymous backer<span ng-if="anonymous > 1">s</span></li></ul>',
+    template: '<h2>Tack till:</h2><ul class="backerlist"><li ng-class="{unpaid: !b.paid }" ng-repeat="b in backers">{{b}}</li><li ng-if="anonymous > 0">and {{anonymous}} anonymous backer<span ng-if="anonymous > 1">s</span></li></ul>',
     scope: {
       backers: '=',
       anonymous: '='
